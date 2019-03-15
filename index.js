d69d1c803cc30ad1ec797b88ae8c97c714b8f41b
pylypkutovoi <pylyp.kutov@gmail.com>
2019-03-15 17:16:02 +0100

    add result and random color

--- index.js
+++ index.js
@@ -1,70 +1,111 @@
-var $start = document.querySelector('#start');
-var $game = document.querySelector('#game');
-var $time = document.querySelector('#time');
+  var $start = document.querySelector('#start');
+  var $game = document.querySelector('#game');
+  var $time = document.querySelector('#time');
+  var $result = document.querySelector('#result');
+  var $timeHeader = document.querySelector('#time-header');
+  var $resultHeader = document.querySelector('#result-header')
+  var $gameTime = document.querySelector('#game-time')
 
-var score = 0;
-var isGameStarted = false;
+
+  var score = 0;
+  var isGameStarted = false;
 
 
-$start.addEventListener('click', startGame);
-$game.addEventListener('click', handleClick);
+  $start.addEventListener('click', startGame);
+  $game.addEventListener('click', handleClick);
+  $gameTime.addEventListener('input', setTime);
 
-function startGame() {
-  isGameStarted = true;
-  $start.classList.add('hide');
-  $game.style.backgroundColor = '#fff';
-
-  var interval = setInterval(function () {
-    var time = parseFloat($time.textContent);
-
-    if(time <=0) {
-      clearInterval(interval);
-      endGame();
-    } else {
-      $time.textContent = (time - 0.1).toFixed(1);
-    }
-  }, 100);
-
-  renderBox();
-}
-
-function endGame() {
-  isGameStarted = false;
-}
-
-function handleClick(e) {
-  if (!isGameStarted) {
-    return
+  function show($el) {
+    $el.classList.remove('hide');
   }
 
-  if (e.target.dataset.box) {
-    score++
+  function hide($el) {
+    $el.classList.add('hide');
+  }
+
+
+  function startGame() {
+    score = 0;
+
+    setTime()
+
+    $gameTime.setAttribute('disabled', 'true')
+
+    isGameStarted = true;
+    hide($start);
+    $game.style.backgroundColor = '#fff';
+
+    var interval = setInterval(function () {
+      var time = parseFloat($time.textContent);
+
+      if(time <=0) {
+        clearInterval(interval);
+        endGame();
+      } else {
+        $time.textContent = (time - 0.1).toFixed(1);
+      }
+    }, 100);
+
     renderBox();
-    
   }
-}
 
-function renderBox() {
-  $game.innerHTML = '';
+  function setScore() {
+    $result.textContent = score.toString();
+  }
 
-  var box = document.createElement('div');
-  var boxSize = getRandom(20, 100);
-  var gameSize = $game.getBoundingClientRect();
-  var maxTop = gameSize.height - boxSize;
-  var maxLeft = gameSize.width - boxSize;
+  function setTime() {
+    var time = +$gameTime.value;
 
-  box.style.height = box.style.width = boxSize + 'px';
-  box.style.position = 'absolute';
-  box.style.backgroundColor ='#000';
-  box.style.borderRadius = '50%'
-  box.style.top = getRandom(0, maxTop) + 'px';
-  box.style.left = getRandom(0, maxLeft)+ 'px';
-  box.style.cursor = 'pointer';
-  box.setAttribute('data-box', 'true');
+    $time.textContent = time.toFixed(1);
+    show($timeHeader);
+    hide($resultHeader);
+  }
 
-  $game.insertAdjacentElement('afterbegin', box);
-}
+  function endGame() {
+    isGameStarted = false;
+    setScore()
 
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max-min) + min)
-}
+    $gameTime.removeAttribute('disabled')
+    $start.classList.remove('hide');
+    $game.innerHTML = '';
+    $game.style.backgroundColor = '#ccc';
+
+    hide($timeHeader);
+    show($resultHeader);
+  }
+
+  function handleClick(e) {
+    if (!isGameStarted) {
+      return
+    }
+    if (e.target.dataset.box) {
+      score++
+      renderBox();
+    }
+  }
+
+  function renderBox() {
+    $game.innerHTML = '';
+
+    var box = document.createElement('div');
+    var boxSize = getRandom(20, 100);
+    var gameSize = $game.getBoundingClientRect();
+    var maxTop = gameSize.height - boxSize;
+    var maxLeft = gameSize.width - boxSize;
+    var color = getRandom(1, 360)
+
+    box.style.height = box.style.width = boxSize + 'px';
+    box.style.position = 'absolute';
+    box.style.backgroundColor = "hsl(" + color + ", 100%, 50%)";
+    box.style.borderRadius = '50%'
+    box.style.top = getRandom(0, maxTop) + 'px';
+    box.style.left = getRandom(0, maxLeft)+ 'px';
+    box.style.cursor = 'pointer';
+    box.setAttribute('data-box', 'true');
+
+    $game.insertAdjacentElement('afterbegin', box);
+  }
+
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max-min) + min)
+  }
