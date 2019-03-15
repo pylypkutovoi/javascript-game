6b2507249c8717fe34825a0e54e4f8cb8717ec16
pylypkutovoi <pylyp.kutov@gmail.com>
2019-03-15 14:30:35 +0100

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+var $start = document.querySelector('#start');
+var $game = document.querySelector('#game');
+
+
+$start.addEventListener('click', startGame);
+
+function startGame() {
+  $start.classList.add('hide');
+  $game.style.backgroundColor = '#fff';
+}
